7c08ed2ea0580f779d17e284ec49c4ff5a148514
Kaiqueoliv3ir4 <kaiquematos321kaka@gmail.com>
2024-12-05 09:57:13 -0300

    atividade aula 2
    valeu valeu

---
+++ aula02.py
@@ -0,0 +1,11 @@
+print('vai corinthians')
+
+nome_usuario = 'vagner love' # home lander
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1.71 # tipo -> float = ponto flutuante
+maior_idade = False # tipo -> bool = boolean
+
+print ( 'seja bem-vindo', nome_usuario)
+print (' sua idade é', idade_usuario, 'anos')
+print('voce tem', altura_usuario,'metros')
+print('voce é maior de idade?', maior_idade)
